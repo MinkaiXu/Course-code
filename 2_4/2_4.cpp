06358c771cc191c0eb2c70ec1aebfde4a8a361c2
mkxu <mkxu@foxmail.com>
2018-10-06 20:03:49 +0800

    双循环链表完成

--- 2_4/2_4.cpp
+++ 2_4/2_4.cpp
@@ -10,7 +10,7 @@ template <class T>
 class List
 {
   private:
-    struct node //双循环链表的节点类
+    struct node //双循环链表的结点类
     {
         T data;            //存放数据元素
         node *prev, *next; //存放直接前驱和直接后继的地址
@@ -32,12 +32,12 @@ class List
     node *head; //头指针
 
   public:
-    List() : head(NULL) {} //创建一个头节点用以初始化双链表
+    List() : head(NULL) {} //创建一个头结点用以初始化双链表
     ~List()
     {
         node *p = head, *q;
 
-        while (p != head)
+        while (p != head) //删除所有结点
         {
             q = p->next;
             delete p;
@@ -45,7 +45,7 @@ class List
         }
     }
 
-    class MyItr
+    class MyItr //迭代器类的定义
     {
         friend List;
 
@@ -59,5 +59,49 @@ class List
         {
             return cur->data;
         }
+
+        MyItr operator++(int x)
+        {
+            MyItr tmp = *this;
+            cur = cur->next;
+            return tmp;
+        }
+
+        MyItr operator++()
+        {
+            return cur = cur->next;
+        }
+
+        MyItr operator--(int x)
+        {
+            MyItr tmp = *this;
+            cur = cur->prev;
+            return tmp;
+        }
+
+        MyItr operator++()
+        {
+            return cur = cur->prev;
+        }
+
+        bool operator==(const MyItr &a)
+        {
+            return cur == a.cur;
+        }
+
+        bool operator!=(const MyItr &a)
+        {
+            return cur != a.cur;
+        }
+
+        bool IsNULL()
+        {
+            return cur == NULL;
+        }
+    };
+
+    MyItr begin() const
+    {
+        return head;
     }
-}
+};
